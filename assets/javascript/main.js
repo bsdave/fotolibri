d150e0e58a0136163264aff134d233c51afd8cd3
Sergei Balabanov <sergeibalabanov@gmail.com>
2019-07-23 22:55:48 +0600

    Add modals to ui kit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,11 @@
 $(function () {
-  
+  $('.open-modal').click(function () {
+    $('body').addClass('overlay');
+    $(`#${$(this).data('modal-id')}`).addClass('opened');
+  });
+
+  $('.close-modal').click(function () {
+    $('body').removeClass('overlay');
+    $(this).parents('.modal-box').removeClass('opened');
+  });
 });
