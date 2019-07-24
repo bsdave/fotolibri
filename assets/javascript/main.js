d81fd43d9cbf4a5c61d54cb8c9fe47f8a6b88aa1
Sergei Balabanov <sergeibalabanov@gmail.com>
2019-07-25 03:47:42 +0600

    Made uploading photos page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,4 +8,35 @@ $(function () {
     $('body').removeClass('overlay');
     $(this).parents('.modal-box').removeClass('opened');
   });
+
+  $('.photo-cart').click(function () {
+    $('.photo-cart.choosed').removeClass('choosed');
+    $(this).toggleClass('choosed');
+    $(this).css('background', $('.color.choosed').css('background'));
+  });
+
+  $('.color').click(function () {
+    $('.color.choosed').removeClass('choosed');
+    $('.photo-cart.choosed').css('background', $(this).css('background'));
+    $(this).addClass('choosed');
+  });
+
+  // TODO remove!! Only for test sample
+  (function myLoop(i) {
+    setTimeout(function () {
+      $('.photo-cart.uploading').find('.percents').text(`${i}%`);  
+      $('.photo-cart.uploading').find('.progress-value').css('width', `${i}%`);   
+      
+      i += Math.round(Math.random(8));
+      
+      if (i <= 100) {
+        myLoop(i)
+      } else {
+        $('.photo-cart.uploading').removeClass('uploading');
+        $('.button.disabled').removeClass('disabled');
+      };
+    }, 30)
+  })(0);
+  // TODO remove!! Only for test sample
+
 });
